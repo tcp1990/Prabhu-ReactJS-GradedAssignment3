9a8a0fdedb1acb5f5cd1ac05ab84b9615b3dcb9d
prabhu <prabhu_tc@yahoo.com>
2023-02-21 08:22:55 +0530

    [Adding/updating files]

--- movies-on-the-tip/src/components/Home.tsx
+++ movies-on-the-tip/src/components/Home.tsx
@@ -5,21 +5,40 @@ import MovieListHeading from './MovieListHeading';
 import SearchBox from './SearchBox';
 import AddFavourites from './AddFavourites';
 import RemoveFavourites from './RemoveFavourites';
-import { getUpcominMovies } from '../services/movies';
+import { getUpcominMovies, getMoviesInTheaters, getTopRatedIndia, getTopRatedMovies } from '../services/movies';
 import IMovieItem from '../models/IMovieItem';
+
+const options = ['Coming Soon', 'Movies in Theaters', 'Top rated Indian', 'Movies in Theaters'];
 
 function Home() {
 
 	const [movies, setMovies] = useState<IMovieItem[]>([]);
 	const [favourites, setFavourites] = useState<IMovieItem[]>([]);
-	const [searchValue, setSearchValue] = useState('');
+	const [searchValue, setSearchValue] = React.useState<string | null>(options[0]);
+	const [inputValue, setInputValue] = React.useState('');
 
 	useEffect(() => {
 		const getMovies = async () => {
 			try {
-				const data = await getUpcominMovies();
-				console.log(data);
-				setMovies(data);
+				switch (searchValue) {
+					case options[0]:
+						const upcomingMovieData = await getUpcominMovies();
+						setMovies(upcomingMovieData);
+						break;
+					case options[1]:
+						const moviesInTheatersData = await getMoviesInTheaters();
+						setMovies(moviesInTheatersData);
+						break;
+					case options[2]:
+						const topRatedIndiaData = await getTopRatedIndia();
+						setMovies(topRatedIndiaData);
+						break;
+					case options[3]:
+						const topRatedMoviesData = await getTopRatedMovies();
+						setMovies(topRatedMoviesData);
+						break;
+
+				}
 			} catch (error) {
 
 			} finally {
@@ -28,7 +47,7 @@ function Home() {
 		};
 
 		getMovies();
-	}, []);
+	}, [searchValue]);
 
 	useEffect(() => {
 		const movieFavourites = JSON.parse(
@@ -61,11 +80,16 @@ function Home() {
 
 
 	return (
-		
+
 		<div className='container-fluid movie-app'>
 			<div className='row d-flex align-items-center mt-4 mb-4'>
 				<MovieListHeading heading='Movies' />
-				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+				<SearchBox
+					options={options}
+					searchValue={searchValue}
+					setSearchValue={setSearchValue}
+					inputValue={inputValue}
+					setInputValue={setInputValue} />
 			</div>
 			<div className='row'>
 				<MovieList
--- movies-on-the-tip/src/components/SearchBox.tsx
+++ movies-on-the-tip/src/components/SearchBox.tsx
@@ -1,12 +1,23 @@
-const SearchBox = (props : any) => {
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+const SearchBox = (props: any) => {
 	return (
 		<div className='col col-sm-4'>
-			<input
-				className='form-control'
-				value={props.value}
-				onChange={(event) => props.setSearchValue(event.target.value)}
-				placeholder='Type to search...'
-			></input>
+			<Autocomplete
+				value={props.searchValue}
+				onChange={(event: any, newValue: string | null) => {
+					props.setSearchValue(newValue);
+				}}
+				inputValue={props.inputValue}
+				onInputChange={(event, newInputValue) => {
+					props.setInputValue(newInputValue);
+				}}
+				id="choose-movies-category"
+				options={props.options}
+				sx={{ width: 300, margin: 1 }}
+				renderInput={(params) => <TextField {...params} label="Choose Category" />}
+			/>
 		</div>
 	);
 };
