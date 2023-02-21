716f9b1aa14e26f64e439d1a4ca877b58a592b2d
prabhu <prabhu_tc@yahoo.com>
2023-02-21 10:13:48 +0530

    [Adding/updating files]

--- movies-on-the-tip/src/models/IMovieItem.ts
+++ movies-on-the-tip/src/models/IMovieItem.ts
@@ -2,6 +2,7 @@ export default interface IMovieItem {
     id: number,
     title: string,
     year: number,
+    poster: string,
     storyline: string,
     imdbRating: string,
     posterurl: string
